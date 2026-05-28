501fe1f6d5f780701609d977af7d92051b096cc5
Aditya Y <108022811+aady1311@users.noreply.github.com>
2026-05-29 00:32:08 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        for(let i = 0; i < nums.length; i++){
+         for(let j = 1 + i; j <nums.length; j++){
+            if(nums[i] + nums[j] === target){
+                return[i, j]
+            }
+         }
+        }
+    }
+}
